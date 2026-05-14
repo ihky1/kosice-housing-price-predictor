ef9a50709e9e84d27fbc54f03e3483dfe9ba040e
Hlib Kyba <hlib.kyba@student.tuke.sk>
2026-05-14 08:11:09 +0200

    feat: area data is now scrapped

--- src/scraper.py
+++ src/scraper.py
@@ -12,6 +12,7 @@ OUTPUT_PATH = ROOT / "data" / "processed" / "kosice_housing_prices_data.csv"
 raw_prices_data = []
 raw_location_data = []
 raw_rooms_data = []
+raw_area_data = []
 
 for page in range(1, NUM_OF_SCRAPPED_PAGES + 1):
     print(f"=== PAGE {page} ===")
@@ -25,6 +26,7 @@ for page in range(1, NUM_OF_SCRAPPED_PAGES + 1):
     page_prices_data = []
     page_location_data = []
     page_rooms_data = []
+    page_area_data = []
 
     for p in all_text:
         if len(p.text) > 6 and "€/mes." in p.text[-6:] and 'mui-pc6no8' in p.parent.parent.get("class"):
@@ -34,22 +36,34 @@ for page in range(1, NUM_OF_SCRAPPED_PAGES + 1):
         if 'mui-16di0u5' in p.get("class") and 'mui-1blo5z7' in p.parent.get("class"):
             page_rooms_data.append(p.text)
 
+    # search data for area if it's there
+    cards = soup.find_all("div", class_="mui-10x79uu")
+    for card in cards:
+        area_el = card.find("div", class_="mui-45ozih")
+        if area_el:
+            page_area_data.append(area_el.find("p").text)
+        else:
+            page_area_data.append(None)
+
     raw_prices_data.extend(page_prices_data)
     raw_location_data.extend(page_location_data)
     raw_rooms_data.extend(page_rooms_data)
+    raw_area_data.extend(page_area_data[::2])
 
     print(f"Price data pieces scrapped: {len(page_prices_data)}")
     print(f"Location data pieces scrapped: {len(page_location_data)}")
     print(f"Room data pieces scrapped: {len(page_rooms_data)}")
+    print(f"Area data pieces scrapped: {len(page_area_data[::2])}")
 
     time.sleep(1)
 
-if not (len(raw_prices_data) == len(raw_location_data) == len(raw_rooms_data)):
+if not (len(raw_prices_data) == len(raw_location_data) == len(raw_rooms_data) == len(raw_area_data)):
     raise ValueError(f"Mismatch: some data is missing.")
 
 # print(raw_prices_data)
 # print(raw_location_data)
 # print(raw_rooms_data)
+# print(raw_area_data)
 
 # == CLEAN DATA ==
 
@@ -81,19 +95,29 @@ for location in raw_location_data:
 ## CLEAN ROOMS
 clean_rooms_data = []
 for rooms in raw_rooms_data:
-    if rooms == "Garsónka" or rooms == "Mezonet":
+    if rooms == "Garsónka" or rooms == "Mezonet" or rooms == "Apartmán":
         clean_rooms_data.append(rooms)
     else:
         clean_rooms_data.append(int(rooms[0]))
 #print(clean_rooms_data)
+
+## CLEAN AREA
+clean_area_data = []
+for area in raw_area_data:
+    if area != None:
+        clean_area_data.append(float(area.replace(" m²", "")))
+    else:
+        clean_area_data.append(None)
 
 ## == EXPORT DATA ==
 data = {
     "price": clean_prices_data,
     "district": clean_district_data,
     "county": clean_county_data,
-    "rooms": clean_rooms_data
+    "rooms": clean_rooms_data,
+    "area": clean_area_data
 }
 
 df = pd.DataFrame(data=data)
+print(df.head(50))
 df.to_csv(OUTPUT_PATH, index=False)
